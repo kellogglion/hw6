4df3f697f7af081b29443e6057ff5a459624e26f
kellogglion <puskoor@kelloggalumni.northwestern.edu>
2021-05-11 23:27:13 -0500

    commit1

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -7,7 +7,7 @@ let fs = require('fs')
 // defines a lambda function
 exports.handler = async function(event) {
   // write the event object to the back-end console
-  console.log(event)
+  // console.log(event)
 
   // read movies CSV file from disk
   let moviesFile = fs.readFileSync(`./movies.csv`)
@@ -16,7 +16,7 @@ exports.handler = async function(event) {
   let moviesFromCsv = await csv(moviesFile)
 
   // write the movies to the back-end console, check it out
-  console.log(moviesFromCsv)
+  // console.log(moviesFromCsv)
 
   // 🔥 hw6: your recipe and code starts here!
   let year = event.queryStringParameters.year
@@ -29,19 +29,36 @@ exports.handler = async function(event) {
     }
   }
   else {
-    let returnValue = {
+    let moviesToReturn = {
       numResults: 0,
       movies: []
     }
 
+
+    // loop through all the movies
+
     for (let i=0; i < moviesFromCsv.length; i++) {
 
+        // store each movie in memory from the CSV
+        let movie = moviesFromCsv[i]
+
+        // check if the movie from the year and genre requested
+
+        if (movie.startYear >= year && movie.genres >= genre) {
+        
+          // add the movie to the Array of movies to return
+          moviesToReturn.movies.push(movie)
+        }
     }
+
+    // add the number of movies to the returned movies object
+
+    moviesToReturn.numResults = moviesToReturn.movies.length
 
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+      body: JSON.stringify(moviesToReturn) // a string of data returned
     }
   }
 }
